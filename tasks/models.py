763c54136dd3151e242fe458f9ed9e5b68674b3d
awisu2 <awisu2@gmail.com>
2020-10-11 14:19:21 +0900

    #6 modelを共有アプリに任せるように対応

--- tasks/models.py
+++ tasks/models.py
@@ -2,15 +2,17 @@ from datetime import datetime
 
 from django.db import models
 
+from share.models import ExModel
 
-class Task(models.Model):
+
+class Task(ExModel):
     choice_text: str = models.CharField(max_length=200)
     votes: int = models.IntegerField(default=0)
     name: str = models.CharField(max_length=200)
     pub_date: datetime = models.DateTimeField("date published")
 
 
-class Note(models.Model):
+class Note(ExModel):
     # taskに紐付いている
     task: int = models.ForeignKey(Task, on_delete=models.CASCADE)
     note: str = models.TextField(blank=True, default="")
